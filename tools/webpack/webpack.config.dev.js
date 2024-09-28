46ce2921b3c1f376486a9b09a00a0b72df4987ea
PhysicksKim <physickskim@gmail.com>
2024-09-29 03:46:03 +0900

    fix : remove debugging green border from buttons in concert and football

--- tools/webpack/webpack.config.dev.js
+++ tools/webpack/webpack.config.dev.js
@@ -30,8 +30,6 @@ module.exports = {
     https: {
       key: fs.readFileSync(path.resolve(__dirname, '../../localhost-key.pem')), // 개인 키 경로
       cert: fs.readFileSync(path.resolve(__dirname, '../../localhost.pem')), // 인증서 경로
-      // key: fs.readFileSync(path.join(__dirname, 'localhost-key.pem')), // 개인 키 경로
-      // cert: fs.readFileSync(path.join(__dirname, 'localhost.pem')), // 인증서 경로
     },
   },
   optimization: {
